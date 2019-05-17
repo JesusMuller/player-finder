2006b458051981eae169f9b6056e1c7018709475
Jesus Müller <jesusmul90@gmail.com>
2019-05-17 03:29:54 -0300

    Adding required prop to section title component

--- src/components/section-title/section-title.jsx
+++ src/components/section-title/section-title.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { string } from 'prop-types';
 
 const SectionTitle = ({ textToShow }) => (
     <h1>{textToShow}</h1>
 );
+
+SectionTitle.propTypes = {
+    textToShow: string.isRequired
+};
 
 export default SectionTitle;
