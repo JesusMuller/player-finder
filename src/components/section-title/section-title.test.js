94f1857e1e940278453e6d1848bcbf81a4272f0d
Jesus Müller <jesusmul90@gmail.com>
2019-05-17 01:44:41 -0300

    Adding ennzyme configuration and first test

---
+++ src/components/section-title/section-title.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SectionTile from './section-title';
+
+it('Should render an <h1> with a text inside', ()=>{
+    const wrapper = shallow(<SectionTile textToShow="dummy title"/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
