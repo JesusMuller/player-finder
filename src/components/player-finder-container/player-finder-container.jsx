d0a291c6b0958b927f952c67dcc4df73eb972c56
Jesus Müller <jesusmul90@gmail.com>
2019-05-17 02:32:16 -0300

    first form component

--- src/components/player-finder-container/player-finder-container.jsx
+++ src/components/player-finder-container/player-finder-container.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SectionTitle from '../section-title';
+import SearchForm from '../search-form';
 import './player-finder-container.css';
 
 const PlayerFinderContainer = () => (
     <div className="section-container">
         <SectionTitle textToShow="Football Player Finder"/>
+        <SearchForm />
     </div>
 );
 
---
+++ src/components/search-form/search-form.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchForm = () => (
+    <div>hola soy el search form</div>
+);
+
+export default SearchForm;
