57cc43d41f2665dbbf75f709dc44de9e9787b5e3
Jesus Müller <jesusmul90@gmail.com>
2019-05-21 16:42:30 -0300

    test for section title component

---
+++ src/components/combo-box/combo-box.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ComboBox from './combo-box';
+
+let setProps = {
+    optionsInitializer: 'position',
+    options: ['Attacking Midfield', 'Central Midfield', 'Centre-Back']
+}
+
+describe('<ComboBox />', () => {
+    it('Should render an <select> as wrapper', () => {
+        const wrapper = shallow(<ComboBox {...setProps} />);
+        expect(wrapper.is('select')).toBe(true);
+    });
+
+    it('Should have as default option the "optionInitializer" prop', () => {
+        const wrapper = shallow(<ComboBox {...setProps} />);
+        expect(wrapper.find('option').get(0).props.children).toEqual('position');
+    });
+
+    it('Should have as first option the first value of "options" prop', () => {
+        const wrapper = shallow(<ComboBox {...setProps} />);
+        expect(wrapper.find('option').get(1).props.children).toEqual('Attacking Midfield');
+    });
+
+    it('Should have a wrapper with class "searchFormComboBox"', () => {
+        const wrapper = shallow(<ComboBox {...setProps} />);
+        expect(wrapper.props().className).toEqual('searchFormComboBox');
+    });
+
+    it('Should call onSubmitHandler prop when an option is selected', () => {
+        const onSubmitHandlerMock = jest.fn();
+        const wrapper = shallow(<ComboBox {...{...setProps, onSubmitHandler: onSubmitHandlerMock}} />);
+        wrapper.simulate('change');
+        expect(onSubmitHandlerMock).toHaveBeenCalledTimes(1);
+    });
+});
--- src/components/section-title/section-title.test.js
+++ src/components/section-title/section-title.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SectionTile from './section-title';
 
-it('Should render an <h1> with a text inside', ()=>{
-    const wrapper = shallow(<SectionTile textToShow="dummy title"/>);
-    expect(wrapper).toMatchSnapshot();
+describe('<SectionTitle />', () => {
+    it('Should render an <h1> with a text inside', () => {
+        const wrapper = shallow(<SectionTile textToShow="dummy title" />);
+        expect(wrapper).toMatchSnapshot();
+    });
 });
-
