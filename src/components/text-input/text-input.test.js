6f12e2bafe40e0eed4e1530c0b672f3ca711b08a
Jesus Müller <jesusmul90@gmail.com>
2019-05-21 17:05:13 -0300

    Adding text input component test

---
+++ src/components/text-input/text-input.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TextInput from './text-input';
+
+describe('<TextInput />', () => {
+    it('Should render an <input>', () => {
+        const wrapper = shallow(<TextInput />);
+        expect(wrapper.find('input')).toHaveLength(1);
+    });
+
+    it('Should render an <input> with as "textInput" class', () => {
+        const wrapper = shallow(<TextInput />);
+        expect(wrapper.find('input').props().className).toEqual('textInput');
+    });
+
+    it('Should render an <input> with a placeHolder when', () => {
+        const wrapper = shallow(<TextInput placeHolder="Player Name" />);
+        expect(wrapper.find('input').props().placeholder).toEqual('Player Name');
+    });
+
+    it('Should render an <span> with a error message if "errorMessage" prop is passed', () => {
+        const wrapper = shallow(<TextInput errorMessage="No results found" />);
+        expect(wrapper.find('span').props().children).toEqual('No results found');
+    });
+});
