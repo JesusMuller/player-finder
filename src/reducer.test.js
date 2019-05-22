57890312769d65e628daf5ed944086b119427bff
Jesus Müller <jesusmul90@gmail.com>
2019-05-22 00:23:09 -0300

    Adding tests for reducer

---
+++ src/reducer.test.js
@@ -0,0 +1,32 @@
+import { reducer } from './reducer';
+
+const initialState = {
+    players: undefined,
+    filters: undefined
+};
+
+describe('Only reducer', () => {
+    it('Should return initial state when action type is not registered', () => {
+        expect(reducer(initialState, { type: "NO_REGISTED_ACTION" })).toEqual(initialState);
+    });
+
+    it('Should return state with players when action type is LOAD_PLAYERS', () => {
+        const players = [{ name: 'rojo', position: 'defensive', age: 26 }];
+        const expectedState = {
+            players: [{ name: 'rojo', position: 'defensive', age: 26 }],
+            filters: undefined
+        };
+
+        expect(reducer(initialState, { type: "LOAD_PLAYERS", players })).toEqual(expectedState);
+    });
+
+    it('Should return state with filters when action type is FILTER_PLAYERS', () => {
+        const filters = { name: '', position: 'keeper', age: 26 };
+        const expectedState = {
+            players: undefined,
+            filters: { name: '', position: 'keeper', age: 26 }
+        };
+
+        expect(reducer(initialState, { type: "FILTER_PLAYERS", filters })).toEqual(expectedState);
+    });
+});
