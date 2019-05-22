a6335921cacc946171bee21e1daaee8175068ae2
Jesus Müller <jesusmul90@gmail.com>
2019-05-22 00:04:29 -0300

    Commenting logger middleware

--- src/store.js
+++ src/store.js
@@ -4,11 +4,14 @@ import thunk from 'redux-thunk';
 import { reducer } from './reducer';
 
 //This logger is just to see store changes in browser console
-const logger = store => next => action => {
+//discomment to launch and and to applyMiddlewarea as parameter
+/*
+    const logger = store => next => action => {
     console.log('dispatching', action)
     let result = next(action)
     console.log('next state', store.getState())
     return result
 };
+*/
 
-export default createStore(reducer, applyMiddleware(logger, thunk));
+export default createStore(reducer, applyMiddleware(thunk));
