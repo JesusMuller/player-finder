a0b376d32713c7b88424cae132e493f96e951b56
Jesus Müller <jesusmul90@gmail.com>
2019-05-22 00:51:13 -0300

    Adding tests for selectors

---
+++ src/mocks/mockPlayers.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        "age": 26,
+        "contractUntil": "2022-06-30",
+        "dateOfBirth": "1993-05-13",
+        "jerseyNumber": 9,
+        "name": "Romelu Lukaku",
+        "nationality": "Belgium",
+        "position": "Centre-Forward"
+    }, {
+        "age": 29,
+        "contractUntil": "2019-06-30",
+        "dateOfBirth": "1990-11-07",
+        "jerseyNumber": 1,
+        "name": "David de Gea",
+        "nationality": "Spain",
+        "position": "Keeper"
+    }, {
+        "age": 32,
+        "contractUntil": "2021-06-30",
+        "dateOfBirth": "1987-02-22",
+        "jerseyNumber": 20,
+        "name": "Sergio Romero",
+        "nationality": "Argentina",
+        "position": "Keeper"
+    }, {
+        "age": 25,
+        "contractUntil": "2020-06-30",
+        "dateOfBirth": "1994-04-12",
+        "jerseyNumber": 3,
+        "name": "Eric Bailly",
+        "nationality": "Cote d'Ivoire",
+        "position": "Centre-Back"
+    }, {
+        "age": 30,
+        "contractUntil": "2019-06-30",
+        "dateOfBirth": "1989-11-22",
+        "jerseyNumber": 12,
+        "name": "Chris Smalling",
+        "nationality": "England",
+        "position": "Centre-Back"
+    }, {
+        "age": 29,
+        "contractUntil": "2019-06-30",
+        "dateOfBirth": "1990-03-20",
+        "jerseyNumber": 5,
+        "name": "Marcos Rojo",
+        "nationality": "Argentina",
+        "position": "Centre-Back"
+    }
+];
---
+++ src/selectors.test.js
@@ -0,0 +1,79 @@
+import { getPlayers } from './selectors';
+import mockPlayers from './mocks/mockPlayers';
+
+describe('Testing only selector', () => {
+    it('Should return all players if filters is undefined', () => {
+        expect(getPlayers(mockPlayers, undefined)).toEqual(mockPlayers);
+    });
+
+    it('Should return all players if filters fields ara empty', () => {
+        const filters = { name: '', position: '', age: '' };
+        expect(getPlayers(mockPlayers, filters)).toEqual(mockPlayers);
+    });
+
+    it('Should return all players that match with name field value', () => {
+        const filters = { name: 'David de Gea', position: '', age: '' };
+        const expectedResponse = [{
+            "age": 29,
+            "contractUntil": "2019-06-30",
+            "dateOfBirth": "1990-11-07",
+            "jerseyNumber": 1,
+            "name": "David de Gea",
+            "nationality": "Spain",
+            "position": "Keeper"
+        }];
+        expect(getPlayers(mockPlayers, filters)).toEqual(expectedResponse);
+    });
+
+    it('Should return all players that match with position field value', () => {
+        const filters = { name: '', position: 'Centre-Forward', age: '' };
+        const expectedResponse = [{
+            "age": 26,
+            "contractUntil": "2022-06-30",
+            "dateOfBirth": "1993-05-13",
+            "jerseyNumber": 9,
+            "name": "Romelu Lukaku",
+            "nationality": "Belgium",
+            "position": "Centre-Forward"
+        }];
+        expect(getPlayers(mockPlayers, filters)).toEqual(expectedResponse);
+    });
+
+    it('Should return all players that match with age field value', () => {
+        const filters = { name: '', position: '', age: '29' };
+        const expectedResponse = [{
+            "age": 29,
+            "contractUntil": "2019-06-30",
+            "dateOfBirth": "1990-11-07",
+            "jerseyNumber": 1,
+            "name": "David de Gea",
+            "nationality": "Spain",
+            "position": "Keeper"
+        }, {
+            "age": 29,
+            "contractUntil": "2019-06-30",
+            "dateOfBirth": "1990-03-20",
+            "jerseyNumber": 5,
+            "name": "Marcos Rojo",
+            "nationality": "Argentina",
+            "position": "Centre-Back"
+        }];
+        expect(getPlayers(mockPlayers, filters)).toEqual(expectedResponse);
+    });
+
+    it('Should return all players that match with mixed filter values', () => {
+        const filters = { name: 'david', position: '', age: '29' };
+        const expectedResponse = [{
+            "age": 29,
+            "contractUntil": "2019-06-30",
+            "dateOfBirth": "1990-11-07",
+            "jerseyNumber": 1,
+            "name": "David de Gea",
+            "nationality": "Spain",
+            "position": "Keeper"
+        }];
+        expect(getPlayers(mockPlayers, filters)).toEqual(expectedResponse);
+    });
+
+
+});
